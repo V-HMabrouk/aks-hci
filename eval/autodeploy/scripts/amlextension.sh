187194d37bda9df4f04921ef8b9c5179e8d26da7
V-HMabrouk <103600850+V-HMabrouk@users.noreply.github.com>
2022-07-07 20:32:59 +0200

    Update amlextension.sh

--- eval/autodeploy/scripts/amlextension.sh
+++ eval/autodeploy/scripts/amlextension.sh
@@ -5,4 +5,4 @@ AZ_CONNECTED_K8S_VERSION='1.2.8'
 
 az extension add --name connectedk8s --version $AZ_CONNECTED_K8S_VERSION
 az extension add --name k8s-extension --version $AZ_K8S_EXTENSION_VERSION
-az k8s-extension az k8s-extension create --name "arcml-extension" --extension-type Microsoft.AzureML.Kubernetes --config enableTraining=True enableInference=True inferenceRouterServiceType=loadBalancer allowInsecureConnections=True inferenceLoadBalancerHA=False --cluster-type connectedClusters --cluster-name $AML_CLUSTER_NAME --resource-group $AML_RESOURCE_GROUP --scope cluster
+az k8s-extension create --name "arcml-extension" --extension-type Microsoft.AzureML.Kubernetes --config enableTraining=True enableInference=True inferenceRouterServiceType=loadBalancer allowInsecureConnections=True inferenceLoadBalancerHA=False --cluster-type connectedClusters --cluster-name $AML_CLUSTER_NAME --resource-group $AML_RESOURCE_GROUP --scope cluster
